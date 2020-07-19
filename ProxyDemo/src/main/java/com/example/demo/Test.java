1d16d27202629ee9d279a87e3a0d4f5124bf3c94
zhenglei <lei.zheng@vphotos.cn>
2020-07-19 11:14:03 +0800

    proxy demo module

---
+++ ProxyDemo/src/main/java/com/example/demo/Test.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+
+
+    public static void main(String[] args){
+        List<Test> list =  new ArrayList<>();
+
+        while (true){
+            list.add(new Test());
+        }
+    }
+}
